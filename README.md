450c5830e8f1a7f163f0d459d1cc71cf07575405
David Tuite <dtuite@gmail.com>
2020-07-23 09:07:31 +0100

    Add rollbar

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 A sample service for testing build tools, displaying in Backstage, and whatever
 else you might need it for.
+
+## Development
+
+First add some secrets to the `.env` file in the root. Then run it locally like this:
+
+```shell
+env $(cat .env | xargs) node index.js
+```
+
+## Deployment
+
+There is a helm chart for deployment to Kubernetes.
--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const Sentry = require('@sentry/node');
+const Rollbar = require('rollbar');
 
 const app = express();
 
-Sentry.init({ dsn: 'https://7018c42dfe4641d38b3880b76e7ad0df@o416326.ingest.sentry.io/5346543' });
+Sentry.init({ dsn: process.env.SENTRY_INGESTION_URL });
 
 app.use(Sentry.Handlers.requestHandler());
 
-// All controllers should live here
-app.get('/', function rootHandler(req, res) {
-  res.end('Hello world!');
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
 });
 
-app.get('/debug-sentry', function mainHandler() {
-  throw new Error('My first Sentry error!');
+// All controllers should live here
+app.get('/', function rootHandler(req, res) {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+  try {
+    throw new Error('My first Sentry error!');
+  } catch (e) {
+    // Do nothing. We wanted an error.
+  }
+
+  res.json({
+    message: 'Error sent to Sentry',
+  });
+});
+
+app.get('/debug-rollbar', function mainHandler(req, res) {
+  rollbar.error('This is a test Rollbar error');
+
+  res.json({
+    message: 'Error sent to Rollbar',
+  });
 });
 
 // The error handler must be before any other error middleware and after all controllers
@@ -27,4 +52,6 @@ app.use(function onError(err, req, res) {
   res.end(res.sentry + "\n");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
