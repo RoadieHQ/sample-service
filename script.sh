58263710358b095ebaa63cb820a31fcef9dc85d0
David Tuite <david@roadie.io>
2024-07-10 15:50:55 +0100

    Update steps

--- script.sh
+++
@@ -1,8 +0,0 @@
-date=$(date +"%Y-%m-%dT%H:%M:%S%z")
-string_list_of_actions=$(ggrep --ignore-case --no-filename -Po 'uses:\s([\w\/\@\-\.]+)' .github/workflows/* | tr '\n' ',')
-
-echo "$string_list_of_actions"
-
-cat << EOF > ./payload.json
-{ "items": [{ "timestamp": "$date", "entity": "component:default/sample-service", "facts": [{ "id": "6f108369-8e28-4dca-a98d-66e02e7d226a", "value": "$string_list_of_actions" } }] }] }
-EOF
